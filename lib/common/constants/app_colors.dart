36c2fafa542367b2f878865d1d562756edfc5489
joaopedrobpj <jpbitencourt2017@gmail.com>
2026-04-07 20:06:34 -0300

    criacao da onboarding_page

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static const Color greenlightOne = Color(0xFF63B5AF);
   static const Color greenlightTwo = Color(0xFF438883);
   static const Color white = Color(0xFFFFFFFF);
+  static const Color iceWhite = Color(0xFFEEF8F7);
 }
